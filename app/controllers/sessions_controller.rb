e6d7e92a929e4b38fa1057577e5d8949e62e0bb2
Jesús Goron <jesus.goron@gmail.com>
2016-02-18 21:36:34 +0000

    Authentication methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    # render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+        flash[:success] = "You have successfully login"
+      redirect_to user_path(user)
+      else
+      flash.now[:danger] = "Something went wrong."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have log out"
+    redirect_to root_path
+  end
+
+end
